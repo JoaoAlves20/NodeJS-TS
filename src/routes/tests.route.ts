2e8d8ce61cdad6b090b4dbc24262bce805f90590
JoaoAlves20 <jvsimoesalves@gmail.com>
2026-04-20 19:40:49 -0300

    refactoring path of tests

--- src/routes/tests.route.ts
+++ src/routes/tests.route.ts
@@ -6,13 +6,8 @@ import TestsController from "../controller/tests.controller.ts";
 
 const router = Router();
 
-// Rota para pegar o Params da URL
 router.get('/:qualquer/:coisa', TestsController.getParams);
-
-// Rota para pegar o Query da URL
 router.get('/query', TestsController.getQuery);
-
-// Rota para pegar o Body da requisição
 router.post('/body', TestsController.getBody);
 
 export default router;
